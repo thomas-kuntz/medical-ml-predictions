a0f8cb3a9d05c6f066584aedee7c2fe3d68084c5
leo <leo@localhost.localdomain>
2024-10-25 18:51:26 +0200

    implementations.py and utilities.py corrected and cleaned. Documentation normalized. there are still todo

--- implementations.py
+++ implementations.py
@@ -4,7 +4,21 @@ from utilities import loss_mse, gradient_mse
 from utilities import gradient_logistic, loss_logistic
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
-    """Linear regression using gradient descent"""
+    """
+    Linear regression using gradient descent with a MSE cost function.
+    
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        initial_w: numpy array of shape (D,). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        w: numpy array of shape (D, ), optimal weight vector at the end of GD.
+        loss: a scalar denoting the mse loss value with the optimal weight vector w.
+    """
+    
     w = initial_w
     for _ in range(max_iters):
         w = w - gamma * gradient_mse(y, tx, w)
@@ -12,10 +26,25 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
 
 
 def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
-    """Linear regression using stochastic gradient descent"""
+    """
+    Linear regression using stochastic gradient descent with a MSE cost function.
+    
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        initial_w: numpy array of shape (D,). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        w: numpy array of shape (D, ), optimal weight vector at the end of GD.
+        loss: a scalar denoting the mse loss value with the optimal weight vector w.
+    """
+    BATCH_SIZE_SGD = 1
+
     w = initial_w
     for _ in range(max_iters):
-        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size=1, num_batches=1, shuffle=True):
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size=BATCH_SIZE_SGD, num_batches=1, shuffle=True):
             w = w - gamma * gradient_mse(minibatch_y, minibatch_tx, w)
     return w, loss_mse(y, tx, w)
 
@@ -30,7 +59,7 @@ def least_squares(y, tx):
 
     Returns:
         w: optimal weights, numpy array of shape(D,), D is the number of features.
-        mse: scalar.
+        loss: a scalar denoting the mse loss value with the optimal weight vector w.
 
     >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
     (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
@@ -40,15 +69,17 @@ def least_squares(y, tx):
 
 
 def ridge_regression(y, tx, lambda_):
-    """Ridge regression using normal equations
+    """Ridge regression using normal equations.
 
     Args:
         y: numpy array of shape (N,), N is the number of samples.
         tx: numpy array of shape (N,D), D is the number of features.
-        lambda_: scalar.
+        lambda_: scalar. #todo préciser ? (ils disent juste scalar dans les exos)
 
     Returns:
         w: optimal weights, numpy array of shape(D,), D is the number of features.
+        loss: a scalar denoting the mse loss value with the optimal weight vector w. # TODO check ed mse vs rmse
+
 
     >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
     array([ 0.21212121, -0.12121212])
@@ -60,12 +91,24 @@ def ridge_regression(y, tx, lambda_):
         tx.T @ tx + 2 * n * lambda_ * np.identity(d),
         tx.T @ y
     )
-    # TODO if there’s a problem here, check that we return the correct loss
-    return w, loss_mse(y, tx, w)
+    
+    return w, loss_mse(y, tx, w) # TODO check ed mse vs rmse
 
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
-    """Logistic regression using gradient descent (y ∈ {0, 1})"""
+    """
+    Logistic regression using gradient descent (y ∈ {0, 1})
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        initial_w: numpy array of shape (D,). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        w: numpy array of shape (D, ), optimal weight vector at the end of GD.
+        loss: a scalar denoting the logistic loss value with the optimal weight vector w.
+    """
     w = initial_w
     for _ in range(max_iters):
         w = w - gamma * gradient_logistic(y, tx, w)
@@ -73,10 +116,24 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
 
 
 def reg_logistic_regression(y, tx, lambda_,initial_w, max_iters, gamma):
-    """Regularized logistic regression using gradient descent (y ∈ {0, 1}, with regularization term λ/2 * ∥w∥^2 )"""
+    """Regularized logistic regression using gradient descent (y ∈ {0, 1}, assuming that the regularization term of the loss is λ * ∥w∥^2)
+    
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        lambda_: scalar. #todo préciser ? (ils disent juste scalar dans les exos)
+        initial_w: numpy array of shape (D,). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        w: numpy array of shape (D, ), optimal weight vector at the end of GD.
+        loss: a scalar denoting the logistic loss value with the optimal weight vector w.
+    """
+    
     w = initial_w
     n = len(tx)
     for _ in range(max_iters):
-        gradient = gradient_logistic(y, tx, w) + n * lambda_ * w
+        gradient = gradient_logistic(y, tx, w) + 2 * lambda_ * w  
         w = w - gamma * gradient
     return w, loss_logistic(y, tx, w)
--- utilities.py
+++ utilities.py
@@ -1,30 +1,31 @@
 import numpy as np
 
 def loss_mse(y, tx, w):
-    """Calculate the loss using either MSE or MAE.
+    """Calculate the loss using the MSE cost function.
 
     Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        w: numpy array of shape=(2,). The vector of model parameters.
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        w: numpy array of shape=(D,). The vector of model parameters.
 
     Returns:
-        the value of the loss (a scalar), corresponding to the input parameters w.
+        loss: a scalar corresponding to the MSE loss.
     """
     e = y - tx @ w
     return 1 / (2 * len(y)) * e.T @ e
 
 
 def gradient_mse(y, tx, w):
-    """Computes the gradient at w.
+    """
+    Computes the gradient at w.
 
     Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        w: numpy array of shape=(2, ). The vector of model parameters.
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        w: numpy array of shape=(D,). The vector of model parameters.
 
     Returns:
-        An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
+        gradient: An numpy array of shape (D, ) containing the gradient of the mse loss at w.
     """
     e = y - tx @ w
     return -(1 / len(y)) * tx.T @ e
@@ -33,28 +34,28 @@ def sigmoid(t):
     """apply sigmoid function on t.
 
     Args:
-        t: scalar or numpy array
+        t: scalar 
 
     Returns:
-        scalar or numpy array
+        image: scalar corresponding to sigmoid(t) 
 
     >>> sigmoid(np.array([0.1]))
     array([0.52497919])
     >>> sigmoid(np.array([0.1, 0.1]))
     array([0.52497919, 0.52497919])
     """
-    return 1 / (1 + np.exp(-t))
+    return np.exp(t) / (1 + np.exp(t))
 
 def loss_logistic(y, tx, w):
     """compute the cost by negative log likelihood.
 
     Args:
-        y:  shape=(N, 1)
-        tx: shape=(N, D)
-        w:  shape=(D, 1)
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        w: numpy array of shape=(D,). The vector of model parameters.
 
     Returns:
-        a non-negative loss
+        loss: the logistic loss given y, tx and w.
 
     >>> y = np.c_[[0., 1.]]
     >>> tx = np.arange(4).reshape(2, 2)
@@ -62,23 +63,24 @@ def loss_logistic(y, tx, w):
     >>> round(loss_logistic(y, tx, w), 8)
     1.52429481
     """
-    assert y.shape[0] == tx.shape[0]
+    assert y.shape[0] == tx.shape[0] #todo useful ?
     assert tx.shape[1] == w.shape[0]
 
-    sigm = sigmoid(tx @ w)
-    log_l = y * np.log(sigm) + (1 - y) * np.log(1 - sigm)
-    return -1 * log_l.mean()
+    loss_vector = np.log(1 + np.exp(-np.multiply(y, tx @ w)))
+    loss = loss_vector.sum()/len(loss_vector)
+    
+    return loss
 
 def gradient_logistic(y, tx, w):
-    """compute the gradient of loss.
+    """compute the gradient of the logistic loss.
 
     Args:
-        y:  shape=(N, 1)
-        tx: shape=(N, D)
-        w:  shape=(D, 1)
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        w: numpy array of shape=(D,). The vector of model parameters.
 
     Returns:
-        a vector of shape (D, 1)
+        gradient: An numpy array of shape (D, ) containing the gradient of the logistic loss at w.
 
     >>> np.set_printoptions(8)
     >>> y = np.c_[[0., 1.]]
