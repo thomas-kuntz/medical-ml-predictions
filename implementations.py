c295980df0656c085f98b5b7a48770b3db5edc13
Thomas Kuntz <tk@thomaskuntz.org>
2024-10-27 11:12:42 +0100

    Format implementations.py with black

--- implementations.py
+++ implementations.py
@@ -3,10 +3,11 @@ from helpers import batch_iter
 from utilities import loss_mse, gradient_mse
 from utilities import gradient_logistic, loss_logistic
 
+
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     """
     Linear regression using gradient descent with a MSE cost function.
-    
+
     Args:
         y: numpy array of shape (N,), N is the number of samples.
         tx: numpy array of shape (N,D), D is the number of features.
@@ -18,7 +19,7 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
         w: numpy array of shape (D, ), optimal weight vector at the end of GD.
         loss: a scalar denoting the mse loss value with the optimal weight vector w.
     """
-    
+
     w = initial_w
     for _ in range(max_iters):
         w = w - gamma * gradient_mse(y, tx, w)
@@ -28,7 +29,7 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
 def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     """
     Linear regression using stochastic gradient descent with a MSE cost function.
-    
+
     Args:
         y: numpy array of shape (N,), N is the number of samples.
         tx: numpy array of shape (N,D), D is the number of features.
@@ -44,7 +45,9 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
 
     w = initial_w
     for _ in range(max_iters):
-        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size=BATCH_SIZE_SGD, num_batches=1, shuffle=True):
+        for minibatch_y, minibatch_tx in batch_iter(
+            y, tx, batch_size=BATCH_SIZE_SGD, num_batches=1, shuffle=True
+        ):
             w = w - gamma * gradient_mse(minibatch_y, minibatch_tx, w)
     return w, loss_mse(y, tx, w)
 
@@ -87,12 +90,9 @@ def ridge_regression(y, tx, lambda_):
     array([0.03947092, 0.00319628])
     """
     n, d = tx.shape
-    w = np.linalg.solve(
-        tx.T @ tx + 2 * n * lambda_ * np.identity(d),
-        tx.T @ y
-    )
-    
-    return w, loss_mse(y, tx, w) # TODO check ed mse vs rmse
+    w = np.linalg.solve(tx.T @ tx + 2 * n * lambda_ * np.identity(d), tx.T @ y)
+
+    return w, loss_mse(y, tx, w)  # TODO check ed mse vs rmse
 
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
@@ -115,9 +115,9 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
     return w, loss_logistic(y, tx, w)
 
 
-def reg_logistic_regression(y, tx, lambda_,initial_w, max_iters, gamma):
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     """Regularized logistic regression using gradient descent (y ∈ {0, 1}, assuming that the regularization term of the loss is λ * ∥w∥^2)
-    
+
     Args:
         y: numpy array of shape (N,), N is the number of samples.
         tx: numpy array of shape (N,D), D is the number of features.
@@ -130,10 +130,10 @@ def reg_logistic_regression(y, tx, lambda_,initial_w, max_iters, gamma):
         w: numpy array of shape (D, ), optimal weight vector at the end of GD.
         loss: a scalar denoting the logistic loss value with the optimal weight vector w.
     """
-    
+
     w = initial_w
     n = len(tx)
     for _ in range(max_iters):
-        gradient = gradient_logistic(y, tx, w) + 2 * lambda_ * w  
+        gradient = gradient_logistic(y, tx, w) + 2 * lambda_ * w
         w = w - gamma * gradient
     return w, loss_logistic(y, tx, w)
