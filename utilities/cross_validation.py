7d1ebdaf6081e2cdbcd6590792d31e37eaa1077a
alejandrocalles <gabriel.jimenezcalles@epfl.ch>
2024-10-28 01:36:59 +0100

    Add documentation for cross validation, change signature of a cross-val function

--- utilities/cross_validation.py
+++ utilities/cross_validation.py
@@ -55,16 +55,16 @@ def k_th_cross_validation(y, x, k_indices, k, train_fn, loss_fn, *args):
     return loss_tr, loss_te
 
 def cross_validation_reg_logistic_regression(y, x, lambdas, initial_w, max_its, gammas, fold_nums, verbose=False):
-    # TODO proper documentation
     """Perform cross validation for regularized logistic regression.
     Args:
         y:          shape=(N,)
         x:          shape=(N,D)
-        k_indices:  2D array returned by build_k_indices()
-        k:          scalar, the k-th fold (N.B.: not to confused with k_fold which is the fold nums)
-        train_fn:   training function, first two positional parameters must be y (training output) and x (training input)
-        loss_fn:    loss function, parameters must be y (test output), x (test input) and w (training weights)
-        *args:      any additional parameters to be passed to the training function
+        lambdas:    list of values of lambda to cross-validate
+        initial_w:  shape=(D,) initial weights
+        max_its:    list of values of max_iters to cross-validate
+        gammas:     list of values of gamma to cross-validate
+        fold_nums:  int, number of folds to perform
+        verbose:    bool, whether to print progress reports or not 
 
 
     Returns:
@@ -76,7 +76,8 @@ def cross_validation_reg_logistic_regression(y, x, lambdas, initial_w, max_its, 
     best_loss = np.inf
     best_hps = dict(lambda_=None, max_it=None, gamma=None)
 
-    # Keep track of losses for each value of each hyper parameter
+    # Keep track of all losses
+    losses = [[[None for _ in gammas] for _ in max_its] for _ in lambdas]
     losses_per_hp = dict(
         lambda_={l: [] for l in lambdas},
         max_it={m: [] for m in max_its},
@@ -84,27 +85,30 @@ def cross_validation_reg_logistic_regression(y, x, lambdas, initial_w, max_its, 
     )
 
     # Consider each combination of hyperparameters
-    for lambda_, max_it, gamma in product(lambdas, max_its, gammas):
-        if verbose:
-            print(f"\nComputing cross validation for lambda={lambda_}\tmax_it={max_it}\tgamma={gamma}")
+    for ilambda_, lambda_ in enumerate(lambdas):
+        for imax_it, max_it in enumerate(max_its):
+            for igamma, gamma in enumerate(gammas):
+                if verbose:
+                    print(f"\nComputing cross validation for lambda={lambda_}\tmax_it={max_it}\tgamma={gamma}")
 
-        # Cross validate the loss
-        loss = 0
-        for k in range(fold_nums):
-            if verbose:
-                print(f"k={k}", end="\t")
-            loss_tr, loss_te = k_th_cross_validation(y, x, k_indices, k, reg_logistic_regression, loss_logistic, lambda_, initial_w, max_it, gamma) 
-            loss += loss_te
-        loss /= fold_nums
-        if verbose:
-            print("\n\tAverage Loss = {:.6f}".format(loss))
+                # Cross validate the loss
+                loss = 0
+                for k in range(fold_nums):
+                    if verbose:
+                        print(f"k={k}", end="\t")
+                    loss_tr, loss_te = k_th_cross_validation(y, x, k_indices, k, reg_logistic_regression, loss_logistic, lambda_, initial_w, max_it, gamma)
+                    loss += loss_te
+                loss /= fold_nums
+                if verbose:
+                    print("\n\tAverage Loss = {:.6f}".format(loss))
 
-        if loss < best_loss:
-            best_loss = loss
-            best_hps = dict(lambda_=lambda_, max_it=max_it, gamma=gamma)
+                if loss < best_loss:
+                    best_loss = loss
+                    best_hps = dict(lambda_=lambda_, max_it=max_it, gamma=gamma)
 
-        losses_per_hp['lambda_'][lambda_] += [loss]
-        losses_per_hp['max_it'][max_it] += [loss]
-        losses_per_hp['gamma'][gamma] += [loss]
+                loss[ilambda_][imax_it][igamma] = loss
+                losses_per_hp['lambda_'][lambda_] += [loss]
+                losses_per_hp['max_it'][max_it] += [loss]
+                losses_per_hp['gamma'][gamma] += [loss]
 
-    return best_loss, best_hps, losses_per_hp
+    return best_loss, best_hps, losses_per_hp, losses
